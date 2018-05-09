34ccf774ad4031b4800d9d951e754d283d1a889a
awfulthings <24832873+awfulthings@users.noreply.github.com>
2018-05-09 12:18:30 +0200

    book data preprocessing

--- DataHelper.js
+++ DataHelper.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var fetch = require('node-fetch');
 var outputFile = './src/data.json';
+var outputFileProcessed = './src/recommendation-data.json';
 const args = process.argv;
 const command = args[2];
 const apiKey = args[3];
@@ -33,17 +34,87 @@ function ProcessMetadata() {
 
   var data = require('./src/data.json');
 
+  /**
+   * Book Schema
+   * @param author string
+   * @param name string
+   * @param pages int
+   * @param type string ['próza', 'drama', 'poezie']
+   * @param categories string
+   * @param theme string
+   * @param mood string ['neutrální', 'šťastný', 'smutný']
+   * @param year int
+   * @param images string ['TRUE', 'FALSE']
+   * @param description string
+   * @param smallThumbnail string
+   * @param thumbnail string
+  */
+
+  var possibleTypes = [];
+  var possibleCategories = [];
+  var possibleMood = [];
+  var possibleThemes = [];
+
   var newData = data.map(function(book, i) {
+    let { pages, type, categories, theme, mood, year, images } = book;
 
-    // nejake podminky
+    pages = (pages > 200) ? 0 : 1;
+    images = (images === 'TRUE') ? 0 : 1;
 
-    return book;
+    // assign MoodId
+    if (possibleMood.indexOf(mood) === -1) {
+      possibleMood.push(mood);
+    }
+    mood = possibleMood.indexOf(mood);
+
+    // assign TypeID
+    if (possibleTypes.indexOf(type) === -1) {
+      possibleTypes.push(type);
+    }
+    type = possibleTypes.indexOf(type);
+
+    // assign ThemeID
+    if (possibleThemes.indexOf(theme) === -1) {
+      possibleThemes.push(theme);
+    }
+    theme = possibleThemes.indexOf(theme);
+
+    // split categories
+    var parsedCategories = categories.split(', ');
+    // assign categoryID
+    var currentCategories = [];
+    parsedCategories.forEach(function(category) {
+      if (possibleCategories.indexOf(category) === -1) {
+        possibleCategories.push(category);
+      }
+      currentCategories.push(possibleCategories.indexOf(category));
+    });
+
+    return {
+      pages,
+      type,
+      categories: currentCategories,
+      theme,
+      mood,
+      images
+    };
 
   });
 
-  fs.writeFile(outputFile, JSON.stringify(newData), function (err) {
+  fs.writeFile(outputFileProcessed, JSON.stringify(newData), function (err) {
     if (err) return console.log(err);
-    console.log('writing to ' + outputFile);
+    console.log('------------------');
+    console.log('possibleCategories:', possibleCategories);
+    console.log('------------------');
+    console.log('possibleTypes:', possibleTypes);
+    console.log('------------------');
+    console.log('possibleMood:', possibleMood);
+    console.log('------------------');
+    var randomIndex = Math.floor(Math.random() * data.length);
+    console.log('Old Data Sample:', data[randomIndex]);
+    console.log('New Data Sample:', newData[randomIndex]);
+    console.log('------------------');
+    console.log('writing to ' + outputFileProcessed);
   });
 
 }
@@ -58,6 +129,7 @@ function FetchMetadata() {
 
   var newData = data.map((async (item, i) => {
       var query = encodeURI(apiUrl+item.name+' '+item.author);
+      console.log(query);
       const res = await fetch(query);
       const json = await res.json();
       let newItem = item;
@@ -98,11 +170,11 @@ function FetchMetadata() {
 
     }));
 
-  console.log('///–––––––––––––––––––––––-///');
-  console.log('///-----Write output-––––-///');
-  console.log('///–––––––––––––––––––––––-///');
-
   Promise.all(newData).then((completed) => {
+
+    console.log('///–––––––––––––––––––––––-///');
+    console.log('///-----Write output-––––-///');
+    console.log('///–––––––––––––––––––––––-///');
 
     fs.writeFile(outputFile, JSON.stringify(completed), function (err) {
       if (err) return console.log(err);
